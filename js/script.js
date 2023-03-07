18036861b3b8760c2f45ffd9d20f33e554a3adb7
amrusalim <salim16.ms21@gmail.com>
2023-03-08 06:48:11 +0700

    slide

--- js/script.js
+++ js/script.js
@@ -14,3 +14,32 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+//slide-show image
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var imgList = document.getElementsByClassName("about-image");
+  if (n > imgList.length) slideIndex = 1;
+  else if (n < 1) slideIndex = imgList.length;
+
+  for (i = 0; i < imgList.length; i++) {
+    imgList[i].style.display = "none";
+  }
+
+  // array dimulai dari 0=0,1,2
+  imgList[slideIndex - 1].style.display = "block";
+}
+
+// set interval( agar bisa slide show)
+setInterval(() => {
+  // ...dipanggil ke sini
+  plusDivs(1);
+}, 2000);
